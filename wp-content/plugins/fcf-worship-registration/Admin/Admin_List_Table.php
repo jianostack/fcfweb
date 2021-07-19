414888c3116765d1c1f209cffaa8eeb94b870229
jianostack <1501270+jianostack@users.noreply.github.com>
2021-07-20 00:18:03 +0800

    Added delete row function to worship registration table.

--- wp-content/plugins/fcf-worship-registration/Admin/Admin_List_Table.php
+++ wp-content/plugins/fcf-worship-registration/Admin/Admin_List_Table.php
@@ -7,150 +7,187 @@ if( ! class_exists( 'WP_List_Table' ) ) {
     require_once( ABSPATH . 'wp-admin/includes/class-wp-list-table.php' );
 }
 
+/**
+ * Custom_Table_Example_List_Table class that will display our custom table
+ * records in nice table
+ */
 class Admin_List_Table extends WP_List_Table
 {
     /**
-     * Prepare the items for the table to process
-     *
-     * @return Void
+     * [REQUIRED] You must declare constructor and give some basic params
      */
-    public function prepare_items()
+    function __construct()
     {
-        $columns = $this->get_columns();
-        $hidden = $this->get_hidden_columns();
-        $sortable = $this->get_sortable_columns();
+        global $status, $page;
 
-        $data = $this->table_data();
-        usort( $data, array( &$this, 'sort_data' ) );
-
-        $perPage = 10;
-        $currentPage = $this->get_pagenum();
-        $totalItems = count($data);
-
-        $this->set_pagination_args( array(
-            'total_items' => $totalItems,
-            'per_page'    => $perPage
-        ) );
-
-        $data = array_slice($data,(($currentPage-1)*$perPage),$perPage);
-
-        $this->_column_headers = array($columns, $hidden, $sortable);
-        $this->items = $data;
+        parent::__construct(array(
+            'singular' => 'person',
+            'plural' => 'persons',
+        ));
     }
 
     /**
-     * Override the parent columns method. Defines the columns to use in your listing table
+     * [REQUIRED] this is a default column renderer
      *
-     * @return Array
+     * @param $item - row (key, value array)
+     * @param $column_name - string (key)
+     * @return HTML
      */
-    public function get_columns()
+    function column_default($item, $column_name)
     {
-        $columns = array(
-            'time' => 'Time',
-            'fullname' => 'Full Name',
-            'email' => 'Email',
-            'phone_number' => 'Phone Number',
-            'service' => 'Service',
-            'session' => 'Session'
+        return $item[$column_name];
+    }
+
+    /**
+     * [OPTIONAL] this is example, how to render specific column
+     *
+     * method name must be like this: "column_[column_name]"
+     *
+     * @param $item - row (key, value array)
+     * @return HTML
+     */
+    function column_age($item)
+    {
+        return '<em>' . $item['age'] . '</em>';
+    }
+
+    /**
+     * [OPTIONAL] this is example, how to render column with actions,
+     * when you hover row "Edit | Delete" links showed
+     *
+     * @param $item - row (key, value array)
+     * @return HTML
+     */
+    function column_name($item)
+    {
+        // links going to /admin.php?page=[your_plugin_page][&other_params]
+        // notice how we used $_REQUEST['page'], so action will be done on curren page
+        // also notice how we use $this->_args['singular'] so in this example it will
+        // be something like &person=2
+        $actions = array(
+            'delete' => sprintf('<a href="?page=%s&action=delete&id=%s">%s</a>', $_REQUEST['page'], $item['id'], __('Delete', 'worship-registration')),
         );
+
+        return sprintf('%s %s',
+            $item['fullname'],
+            $this->row_actions($actions)
+        );
+    }
+
+    /**
+     * [REQUIRED] this is how checkbox column renders
+     *
+     * @param $item - row (key, value array)
+     * @return HTML
+     */
+    function column_cb($item)
+    {
+        return sprintf(
+            '<input type="checkbox" name="id[]" value="%s" />',
+            $item['id']
+        );
+    }
+
+    /**
+     * [REQUIRED] This method return columns to display in table
+     * you can skip columns that you do not want to show
+     * like content, or description
+     *
+     * @return array
+     */
+    function get_columns()
+    {
+
+      $columns = array(
+        'name' => __('Name', 'worship-registration'),
+        'time' => __('Date Added', 'worship-registration'),
+        'email' => __('E-Mail', 'worship-registration'),
+        'phone_number' => __('Phone Number', 'worship-registration'),
+        'service' => __('Service', 'worship-registration'),
+        'session' => __('Session', 'worship-registration')
+      );
 
         return $columns;
     }
 
     /**
-     * Define which columns are hidden
+     * [OPTIONAL] This method return columns that may be used to sort table
+     * all strings in array - is column names
+     * notice that true on name column means that its default sort
      *
-     * @return Array
+     * @return array
      */
-    public function get_hidden_columns()
+    function get_sortable_columns()
     {
-        return array();
-    }
-
-    /**
-     * Define the sortable columns
-     *
-     * @return Array
-     */
-    public function get_sortable_columns()
-    {
-        return array(
-          'session' => array('session', false),
-          'number' => array('number', false),
-          'time' => array('time', false),
-          'email' => array('email', false)
+        $sortable_columns = array(
+            'time' => array('time', true),
+            'session' => array('session', false),
+            'service' => array('service', false)
         );
+        return $sortable_columns;
     }
 
     /**
-     * Get the table data
-     *
-     * @return Array
+     * [OPTIONAL] This method processes bulk actions
+     * it can be outside of class
+     * it can not use wp_redirect coz there is output already
+     * in this example we are processing delete action
+     * message about successful deletion will be shown on page in next part
      */
-    private function table_data()
+    function process_bulk_action()
     {
         global $wpdb;
+        $table_name = $wpdb->prefix . 'worship_registration'; // do not forget about tables prefix
 
-        $table_name = $wpdb->prefix . 'worship_registration';
-        $data = $wpdb->get_results("SELECT * FROM $table_name WHERE is_del = 0", ARRAY_A);
+        if ('delete' === $this->current_action()) {
+            $ids = isset($_REQUEST['id']) ? $_REQUEST['id'] : array();
+            if (is_array($ids)) $ids = implode(',', $ids);
 
-        return $data;
-    }
-
-    /**
-     * Define what data to show on each column of the table
-     *
-     * @param  Array $item        Data
-     * @param  String $column_name - Current column name
-     *
-     * @return Mixed
-     */
-    public function column_default( $item, $column_name )
-    {
-        switch( $column_name ) {
-            case 'time':
-            case 'fullname':
-            case 'email':
-            case 'phone_number':
-            case 'service':
-            case 'session':
-                return $item[ $column_name ];
-
-            default:
-                return print_r( $item, true ) ;
+            if (!empty($ids)) {
+                $wpdb->query("DELETE FROM $table_name WHERE id IN($ids)");
+            }
         }
     }
 
     /**
-     * Allows you to sort the data by the variables set in the $_GET
+     * [REQUIRED] This is the most important method
      *
-     * @return Mixed
+     * It will get rows from database and prepare them to be showed in table
      */
-    private function sort_data( $a, $b )
+    function prepare_items()
     {
-        // Set defaults
-        $orderby = 'time';
-        $order = 'asc';
+        global $wpdb;
+        $table_name = $wpdb->prefix . 'worship_registration'; // do not forget about tables prefix
 
-        // If orderby is set, use this as the sort column
-        if(!empty($_GET['orderby']))
-        {
-            $orderby = $_GET['orderby'];
-        }
+        $per_page = 10; // constant, how much records will be shown per page
 
-        // If order is set use this as the order
-        if(!empty($_GET['order']))
-        {
-            $order = $_GET['order'];
-        }
+        $columns = $this->get_columns();
+        $hidden = array();
+        $sortable = $this->get_sortable_columns();
 
-        $result = strcmp( $a[$orderby], $b[$orderby] );
+        // here we configure table headers, defined in our methods
+        $this->_column_headers = array($columns, $hidden, $sortable);
 
-        if($order === 'asc')
-        {
-            return $result;
-        }
+        // [OPTIONAL] process bulk action if any
+        $this->process_bulk_action();
 
-        return -$result;
+        // will be used in pagination settings
+        $total_items = $wpdb->get_var("SELECT COUNT(id) FROM $table_name");
+
+        // prepare query params, as usual current page, order by and order direction
+        $paged = isset($_REQUEST['paged']) ? max(0, intval($_REQUEST['paged'] - 1) * $per_page) : 0;
+        $orderby = (isset($_REQUEST['orderby']) && in_array($_REQUEST['orderby'], array_keys($this->get_sortable_columns()))) ? $_REQUEST['orderby'] : 'time';
+        $order = (isset($_REQUEST['order']) && in_array($_REQUEST['order'], array('asc', 'desc'))) ? $_REQUEST['order'] : 'asc';
+
+        // [REQUIRED] define $items array
+        // notice that last argument is ARRAY_A, so we will retrieve array
+        $this->items = $wpdb->get_results($wpdb->prepare("SELECT * FROM $table_name ORDER BY $orderby $order LIMIT %d OFFSET %d", $per_page, $paged), ARRAY_A);
+
+        // [REQUIRED] configure pagination
+        $this->set_pagination_args(array(
+            'total_items' => $total_items, // total items defined above
+            'per_page' => $per_page, // per page constant defined at top of method
+            'total_pages' => ceil($total_items / $per_page) // calculate pages count
+        ));
     }
 }
